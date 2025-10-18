34b02c9794433642c3d6daeb92ac45b465da9973
WZK10 <dmychael08@gmail.com>
2025-10-18 14:50:38 -0300

    Add files via upload

--- scr/main.tsx
+++ scr/main.tsx
@@ -1,9 +1,25 @@
 
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <App />,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+      <Toaster 
+        position="top-right"
+        toastOptions={{
+          duration: 4000,
+          style: {
+            background: '#363636',
+            color: '#fff',
+          },
+        }}
+      />
+    </BrowserRouter>
+  </React.StrictMode>,
 )
-
