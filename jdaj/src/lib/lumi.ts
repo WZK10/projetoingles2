4a29eacedf792b0fa3649c08484afd349e94adf6
WZK10 <dmychael08@gmail.com>
2025-10-18 15:56:35 -0300

    Atualizacao final do projeto - Sincronizacao de codigo local

--- jdaj/src/lib/lumi.ts
+++
@@ -1,9 +0,0 @@
-
-import { createClient } from '@lumi.new/sdk'
-
-export const lumi = createClient({
-  projectId: 'p369980836752089088',
-  apiBaseUrl: 'https://api.lumi.new',
-  authOrigin: 'https://auth.lumi.new',
-})
-
